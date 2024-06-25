5cb343118e2e3b72cb4d9d28f32528fd32cfc7f7
Felipe Adeildo <oie.eu.sou.um@gmail.com>
2024-06-25 18:22:12 -0300

    feat: Add instagram constants

---
+++ src/instagram/constants.py
@@ -0,0 +1,9 @@
+INSTAGRAM_AJAX = 1014458754
+"""The instagram ajax session ID"""
+
+INSTGARAM_ASBD_ID = 129477
+"""The instagram ASBD ID"""
+
+
+INSTAGRAM_APP_ID = "936619743392459"
+"""The instagram app ID"""
