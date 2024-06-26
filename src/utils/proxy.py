48f0c98eaeb2e194d25126ae78c0006fd618680d
Felipe Adeildo <felipeadeildo@duck.com>
2024-06-26 14:36:07 -0300

    refactor: Remove unecessary dataclass for proxy connection

--- src/utils/proxy.py
+++ src/utils/proxy.py
@@ -1,26 +1,9 @@
 import random
-from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
 
-@dataclass
-class Proxy:
-    """Proxy credentials data representation"""
-
-    ip: str
-    port: str
-    username: Optional[str] = None
-    password: Optional[str] = None
-
-    def __str__(self):
-        if self.username is None or self.password is None:
-            return f"https://{self.ip}:{self.port}"
-        else:
-            return f"https://{self.username}:{self.password}@{self.ip}:{self.port}"
-
-
-def get_random_proxy(config: dict) -> Optional[Proxy]:
+def get_random_proxy(config: dict) -> Optional[str]:
     """Gets a random proxy credentials from the proxy file set in the config
 
     Args:
@@ -32,6 +15,4 @@ def get_random_proxy(config: dict) -> Optional[Proxy]:
     proxies = proxy_path.read_text().split("\n")
     if not proxies:
         return None
-    keys = Proxy.__dataclass_fields__.keys()
-    proxy = random.choice(proxies).split(":")
-    return Proxy(**dict(zip(keys, proxy)))
+    return random.choice(proxies)
